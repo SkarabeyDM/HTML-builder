3350c44eb05da384a773c645e26cec68c06be1fd
Jamal Muslimov <mechanoid0415@gmail.com>
2022-11-07 19:59:37 +0300

    feat: solve problem 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+const { stdin, stdout } = process
+
+const file = path.join(__dirname, "text.txt");
+
+// Launch
+fs.writeFile(file, "", (err) => {
+   if (err) throw err;
+   console.log("Введите строку:")
+})
+
+// Events
+process.on('exit', () => stdout.write("Удачи!"));
+process.on('SIGINT', () => process.exit());
+
+// User input
+stdin.on("data", data => {
+   const key = data.toString().toLowerCase().trim();
+   if (key === "exit")
+      process.exit()
+   else
+      fs.appendFile(file, data, (err) => {
+         if (err) throw err;
+      })
+})
