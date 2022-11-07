15ba99b8e675098bb3cc80fd5d47ad355809db16
Jamal Muslimov <mechanoid0415@gmail.com>
2022-11-07 18:02:29 +0300

    feat: solve problem 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,7 @@
+const fs = require("fs");
 const path = require("path");
+
+const file = path.join(__dirname, "text.txt");
+const readStream = fs.createReadStream(file, "utf8");
+
+readStream.on("data", chunk => console.log(chunk));
