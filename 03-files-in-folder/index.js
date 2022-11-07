8eb348344d723f4398c9f12888e822c9b41e9386
Jamal Muslimov <mechanoid0415@gmail.com>
2022-11-07 23:57:17 +0300

    feat: solve problem 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,39 @@
+const promises = require("fs/promises");
+const path = require("path");
+
+const dir = path.join(__dirname, "secret-folder");
+
+const pathToFile = (fileName) => path.join(dir, fileName);
+// Разделяет имя файла и разрешение
+const fileExt = (fileName) => fileName.match(/^(.+)\.([0-9a-z]+)$/).slice(1)
+// Записывает данные о файле в строку
+const fileStats = async (fileName) => [...fileExt(fileName), (await promises.stat(pathToFile(fileName))).size / 1000 + "kb"].join(" - ");
+
+// Вызов анонимной функции запускающей анонимную асинхронную функцию
+(async () => {
+   const files = await promises.readdir(dir, { withFileTypes: true });
+
+   files.forEach(async file => {
+      if (file.isFile()) {
+         const stats = await fileStats(file.name)
+         console.log(stats)
+      }
+   })
+})()
+
+
+// Старый вариант, до рефактора:
+
+//const parseFileName = (fileName) => fileName.split(".");
+
+// (async () => {
+//    let files = await promises.readdir(dir, { withFileTypes: true });
+//    files = await Promise.all(files
+//       // Удаление папок
+//       .filter(file => file.isFile())
+//       // Читаемый вид
+//       .map(async ({ name: fileName }) => [...parseFileName(fileName), (await promises.stat(pathToFile(fileName))).size / 1000 + "kb"].join(" - ")))
+
+//    // Вывод в консоль
+//    files.forEach(file => console.log(file))
+// })()
