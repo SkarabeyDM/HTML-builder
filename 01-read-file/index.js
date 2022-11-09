62365b189961643b386b1a00ed538c5af3388b5e
Jamal Muslimov <mechanoid0415@gmail.com>
2022-11-09 12:26:25 +0300

    refactor: simplify output function

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,4 +4,4 @@ const path = require("path");
 const file = path.join(__dirname, "text.txt");
 const readStream = fs.createReadStream(file, "utf8");
 
-readStream.on("data", chunk => console.log(chunk));
+readStream.on("data", console.log);
