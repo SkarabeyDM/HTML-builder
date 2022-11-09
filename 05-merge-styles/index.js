93f2cf2f5f98b8a5e628dcce30cdaa1c3f32f9ac
Jamal Muslimov <mechanoid0415@gmail.com>
2022-11-09 23:50:18 +0300

    refactor: small changes is 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,15 +5,15 @@ const path = require("path")
 const stylesDir = path.resolve(__dirname, "./styles")
 const targetFile = path.resolve(__dirname, "./project-dist/bundle.css")
 
-mergeFiles();
+mergeFiles(stylesDir, targetFile);
 
-async function mergeFiles() {
-   const writeStream = createWriteStream(targetFile)
+async function mergeFiles(fromDir, toFile) {
+   const writeStream = createWriteStream(toFile)
 
    const isCss = (str) => str.split(".").pop() === "css"
-   const stylePath = (str) => path.join(stylesDir, str)
+   const stylePath = (str) => path.join(fromDir, str)
    const merge = async (fileName) => createReadStream(stylePath(fileName)).pipe(writeStream)
 
-   await Promise.all((await readdir(stylesDir)).filter(isCss).map(merge))
+   await Promise.all((await readdir(fromDir)).filter(isCss).map(merge))
 }
 
